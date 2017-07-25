a0fa4f31c8ef561999a444aa2756df292b79286a
qi.shang <qi.shang@bianlifeng.com>
2017-07-25 22:44:48 +0800

    添加配置

--- freshness-service/src/main/java/com/opc/freshness/service/integration/impl/ProductServiceImpl.java
+++ freshness-service/src/main/java/com/opc/freshness/service/integration/impl/ProductServiceImpl.java
@@ -8,10 +8,15 @@ import com.wormpex.cvs.product.api.bean.BeeProduct;
 import com.wormpex.cvs.product.api.bean.BeeProductDetail;
 import com.wormpex.cvs.product.api.bean.BeeShopProduct;
 import com.wormpex.cvs.product.api.remote.ProductRemote;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
-import java.util.*;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 /**
@@ -19,6 +24,8 @@ import java.util.stream.Collectors;
  */
 @Service
 public class ProductServiceImpl implements ProductService {
+    private final static Logger logger = LoggerFactory.getLogger(ProductServiceImpl.class);
+
     @Resource
     private ProductRemote productRemote;
 
@@ -46,6 +53,7 @@ public class ProductServiceImpl implements ProductService {
         try {
             return productRemote.queryProductDetailByBarcode(barcode);
         } catch (RuntimeException e) {
+            logger.error("queryProductDetailByBarcode failed...",e);
             throw new BizException("未查找到产品");
         }
     }
@@ -53,9 +61,9 @@ public class ProductServiceImpl implements ProductService {
     @Override
     public BeeProduct queryByCode(String productCode) {
         List<BeeProduct> products = productRemote.queryByProductCodeList(Lists.newArrayList(productCode));
-        if (CollectionUtils.isEmpty(products)){
+        if (CollectionUtils.isEmpty(products)) {
             throw new BizException("未查找到对应商品");
-        }else {
+        } else {
             return products.get(0);
         }
     }
